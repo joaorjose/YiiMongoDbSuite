250ca10b9cd78648ac891b39248928f6d9ccd52a
Volovikov Ivan <i.volovikov@gmail.com>
2012-08-17 16:08:40 +0400

    fix dataprovider sor and labels

--- EMongoDocument.php
+++ EMongoDocument.php
@@ -1362,4 +1362,14 @@ abstract class EMongoDocument extends EMongoEmbeddedDocument
 		return $criteria;
 	}
 
+    /**
+	 * Checks whether this AR has the named attribute
+	 * @param string $name attribute name
+	 * @return boolean whether this AR has the named attribute (table column).
+	 */
+	public function hasAttribute($name)
+	{
+		return isset($this->$name);
+	}
+
 }
